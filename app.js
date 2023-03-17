c17d5dd140b78926a479a05168c40d57c2833c17
Spark <piotr.rac@gmail.com>
2023-03-17 23:41:05 +0100

    commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sa
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];  // declare item in global scope (moved from post method)
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -27,7 +28,7 @@ app.get("/", function(req, res) {
 
     //szuka plku list.ejs w folderze views
     res.render("list", {
-        kindOfDay: day,
+        listTitle: day,
         newListItems: items
     });   
 
@@ -35,14 +36,35 @@ app.get("/", function(req, res) {
  
 app.post("/", function(req, res) {
 
-    var item = req.body.txt1;
-    items.push(item);
-    //console.log(num1);
-    //res.send(num1);
+    //console.log(req.body);
 
-    res.redirect("/"); //redirect item to home route, which will render new item through res.render
+    let item = req.body.newItem;
 
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/"); //redirect item to home route, which will render new item through res.render
+    }
 });
+
+app.get("/work", function(req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });   
+});
+
+app.get("/about", function(req, res) {
+  return res.render("/about");
+});
+
+//app.post("/work", function(req,res) {
+    //let item = req.body.newItem;
+    //workItems.push(item);
+    //res.redirect("/work");
+//});
 
 app.listen(port, function() {
     console.log("Server started on port " + port);
